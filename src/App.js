79a2a5b357a5eb90137b47a604e61d746ed20077
AdityaHirapara <adityahirapara2016@gmail.com>
2018-07-13 22:23:48 +0530

    basic ui structure :art:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Sidebar from './Sidebar.js';
+import Main from './Main.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Sidebar />
+        <Main />
       </div>
     );
   }
