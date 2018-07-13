91cd9c77346cba9eda94df328fa42243992d9223
AdityaHirapara <adityahirapara2016@gmail.com>
2018-07-13 23:25:09 +0530

    render sidebar with contacts data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import Sidebar from './Sidebar.js';
 import Main from './Main.js';
 import './App.css';
+import store from './store';
+import _ from 'lodash';
 
 class App extends Component {
+
   render() {
+    const { contacts } = store.getState();
+
     return (
       <div className="App">
-        <Sidebar />
+        <Sidebar contacts={_.values(contacts)} />
         <Main />
       </div>
     );
