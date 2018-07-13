fd96d1958ed1324a267374dc8f75b88a28666e14
AdityaHirapara <adityahirapara2016@gmail.com>
2018-07-14 01:18:55 +0530

    change file structure of project :building_construction:

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Sidebar from './Sidebar.js';
-import Main from './Main.js';
+import Sidebar from '../components/Sidebar.js';
+import Main from '../components/Main.js';
 import './App.css';
-import store from './store';
+import store from '../store';
 import _ from 'lodash';
 
 class App extends Component {
