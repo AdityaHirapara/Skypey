c641fadebabce244f94347fa0dc7d230540e9a6a
AdityaHirapara <adityahirapara2016@gmail.com>
2018-07-14 13:57:21 +0530

    fix:bug: : chat window should automatically scroll down

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -9,9 +9,26 @@ const Chat = ({ message }) => {
 };
 
 class Chats extends Component {
+  constructor(props) {
+    super(props);
+    this.chatsRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom = () => {
+    this.chatsRef.current.scrollTop = this.chatsRef.current.scrollHeight;
+  };
+
   render() {
     return (
-      <div className="Chats">
+      <div className="Chats" ref={this.chatsRef}>
         {this.props.messages.map(message => (
           <Chat message={message} key={message.number} />
         ))}
--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,6 +1,7 @@
 import { getMessages } from "../static-data";
 import { SEND_MESSAGE } from "../constants/action-types";
 import _ from 'lodash';
+
 export default function messages(state = getMessages(10), action) {
   switch (action.type) {
     case SEND_MESSAGE:
