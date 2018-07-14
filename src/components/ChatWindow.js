c10542d036c78bcbfb96acc47970be2269fe2de7
AdityaHirapara <adityahirapara2016@gmail.com>
2018-07-14 12:59:29 +0530

    design input field :art:

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -2,6 +2,7 @@ import React from "react";
 import store from "../store";
 import Header from "../components/Header";
 import Chats from "../components/Chats";
+import MessageInput from "../containers/MessageInput";
 import "./ChatWindow.css";
 import _ from "lodash";
 
@@ -9,11 +10,13 @@ const ChatWindow = ({ activeUserId }) => {
   const state = store.getState();
   const activeUser = state.contacts[activeUserId];
   const activeMsgs = state.messages[activeUserId];
+  const { typing } = state;
 
   return (
     <div className="ChatWindow">
       <Header user={activeUser} />
       <Chats messages={_.values(activeMsgs)} />
+      <MessageInput value={typing} />
     </div>
   );
 };
--- src/reducers/typing.js
+++ src/reducers/typing.js
@@ -1,7 +1,10 @@
 import { SET_TYPING_VALUE } from "../constants/action-types";
+
 export default function typing(state = "", action) {
-switch (action.type) {
-case SET_TYPING_VALUE:
-return action.payload;
-default:
-return state;
+  switch (action.type) {
+    case SET_TYPING_VALUE:
+      return action.payload;
+    default:
+      return state;
+  }
+}
