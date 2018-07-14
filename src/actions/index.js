ff51380d3d39fafbc27e4e5e6a08dc52ea01de5a
AdityaHirapara <adityahirapara2016@gmail.com>
2018-07-14 13:39:57 +0530

    append new messages :envelope: :inbox_tray:

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import {
   SET_ACTIVE_USER_ID,
-  SET_TYPING_VALUE
+  SET_TYPING_VALUE,
+  SEND_MESSAGE
 } from "../constants/action-types";
 
 export const setActiveUserId = id => ({
@@ -11,4 +12,12 @@ export const setActiveUserId = id => ({
 export const setTypingValue = value => ({
   type: SET_TYPING_VALUE,
   payload: value
+});
+
+export const sendMessage = (message, userId) => ({
+  type: SEND_MESSAGE,
+  payload: {
+    message,
+    userId
+  }
 });
--- src/containers/MessageInput.js
+++ src/containers/MessageInput.js
@@ -1,15 +1,23 @@
 import React from "react";
 import store from "../store";
-import { setTypingValue } from "../actions";
+import { setTypingValue, sendMessage } from "../actions";
 import "./MessageInput.css";
 
 const MessageInput = ({ value }) => {
+  const state = store.getState();
+
   const handleChange = e => {
     store.dispatch(setTypingValue(e.target.value));
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { typing, activeUserId } = state;
+    store.dispatch(sendMessage(typing, activeUserId));
+  };
+
   return (
-    <form className="Message">
+    <form className="Message" onSubmit={handleSubmit}>
       <input
         className="Message__input"
         onChange={handleChange}
