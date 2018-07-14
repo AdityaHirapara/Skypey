eef677f80e285af859a898d7ca7d2ec577e596a3
AdityaHirapara <adityahirapara2016@gmail.com>
2018-07-14 12:35:00 +0530

    fetch messages from store:mailbox_with_mail: :incoming_envelope:

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,13 +1,18 @@
 import React from "react";
 import store from "../store";
 import Header from "../components/Header";
+import Chats from "../components/Chats";
+import _ from "lodash";
 
 const ChatWindow = ({ activeUserId }) => {
   const state = store.getState();
   const activeUser = state.contacts[activeUserId];
+  const activeMsgs = state.messages[activeUserId];
+
   return (
     <div className="ChatWindow">
       <Header user={activeUser} />
+      <Chats messages={_.values(activeMsgs)} />
     </div>
   );
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import user from "./user";
+import messages from "./messages";
 import contacts from "./contacts";
 import activeUserId from "./activeUserId";
 import { combineReducers } from "redux";
 
 export default combineReducers({
   user,
+  messages,
   contacts,
   activeUserId,
 });
